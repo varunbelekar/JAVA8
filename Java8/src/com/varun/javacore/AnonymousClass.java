eed9cc8c0900cba56c807c1551b6a59e66fb2d92
varun belekar <vbele@nets.eu>
2019-12-10 16:38:30 +0530

    Using Anonymous class with ArrayList and HashMap

--- Java8/src/com/varun/javacore/AnonymousClass.java
+++ Java8/src/com/varun/javacore/AnonymousClass.java
@@ -1,5 +1,9 @@
 package com.varun.javacore;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 abstract class Person{
 	abstract public void eat();
@@ -52,6 +56,25 @@ public class AnonymousClass {
 			}
 		};
 		a.run();
+		
+		Integer localVariable1 = 10;
+	    Integer localVariable2 = 10;
+	    Integer localVariable3 = 10;
+	    Map<String, Integer> map = new HashMap<String, Integer>() {
+	      {
+	        put("a", localVariable1);
+	        put("b", localVariable2);
+	        put("c", localVariable3);
+	      }
+	    };
+	    
+	    List<Integer> list = new ArrayList<Integer>(){
+	    	{
+	    		add(3);
+	    		add(5);
+	    	}
+	    };
+	    list.forEach(System.out::println);
 	}
 
 }
