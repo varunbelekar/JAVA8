5c0d6c5501badee4c80c3d2ca63b436382737ff9
varun belekar <vbele@nets.eu>
2020-04-10 12:08:20 +0300

    Java 8 practice lamdas,streams

--- Java8/src/com/varun/test/Test4.java
+++ Java8/src/com/varun/test/Test4.java
@@ -1,37 +1,23 @@
 package com.varun.test;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+class Prime{
+	public void checkPrime(Integer...n){
+		java.util.stream.Stream.of(n)
+			  .filter( i -> new BigInteger(String.valueOf(i)).isProbablePrime(1))
+			  .forEach((j) -> System.out.print(j + " "));
+	}
+	
+}
+
 public class Test4 {
 	
-	public static int findSmallestPosNum(List<Integer> list){
-		
-		for (int i = 0; i < list.size(); i++) {
-			if(list.get(i) != i+1){
-				return i+1;
-			}
-		}
-		return list.size() + 1;
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] a = {0,1,2,3,4};
-		List<Integer> list = new ArrayList();
-		
-		for(int i : a){
-			if(i>0){
-				list.add(i);
-			}
-		}
-		
-		Collections.sort(list);
-		System.out.println(list);
-		System.out.println(findSmallestPosNum(list));
-		
-		//System.out.println(min);
+		new Prime().checkPrime(2, 3, 5, 6);
 	}
 
 }
